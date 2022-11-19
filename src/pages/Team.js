9f120a835c29aef3f76c4642bd2e07e6c7bc1ea4
Jojo <jojothomas1515@gmail.com>
2022-11-19 20:03:48 +0100

    asset and styles moved to components

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/components/Team.css";
+import "../components/team/styles/components/Team.css";
 import { Card } from "../components/team/Card";
 
 
@@ -11,15 +11,15 @@ function Team(props) {
         <p>Get to know the face behind the product.</p>
 
         <div className="team__flex">
-          <Card img_url={require("../assets/team/IMG-20221119-WA0004.jpg")} name={"Alex Chika"}
+          <Card img_url={require("../components/team/assets/team/IMG-20221119-WA0004.jpg")} name={"Alex Chika"}
                 position={"Front-End Dev"} />
-          <Card img_url={require("../assets/team/IMG-20221119-WA0005.jpg")} name={"Busayo Olushola"}
+          <Card img_url={require("../components/team/assets/team/IMG-20221119-WA0005.jpg")} name={"Busayo Olushola"}
                 position={"DevOps"} />
-          <Card img_url={require("../assets/team/IMG-20221119-WA0006.jpg")} name={"Andrew Glado"}
+          <Card img_url={require("../components/team/assets/team/IMG-20221119-WA0006.jpg")} name={"Andrew Glado"}
                 position={"Backend"} />
-          <Card img_url={require("../assets/team/IMG-20221119-WA0007.jpg")} name={"Okolo Enemona"}
+          <Card img_url={require("../components/team/assets/team/IMG-20221119-WA0007.jpg")} name={"Okolo Enemona"}
                 position={"Product Designer"} />
-          <Card img_url={require("../assets/team/IMG-20221119-WA0008.jpg")} name={"Iruoma Valerie Oby-Ezeani"}
+          <Card img_url={require("../components/team/assets/team/IMG-20221119-WA0008.jpg")} name={"Iruoma Valerie Oby-Ezeani"}
                 position={"Project Manager"} />
         </div>
       </div>
