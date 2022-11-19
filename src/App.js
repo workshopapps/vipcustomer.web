bbd275dcaf5c9ef9394c936edd28ccf84bce84c1
Jojo <jojothomas1515@gmail.com>
2022-11-19 19:42:21 +0100

    meet the team page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { Themes, GLobalStyles } from "./theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {ThemeProvider} from "styled-components";
+import {GLobalStyles, Themes} from "./theme";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 // Stores
-import { Store } from "./store/contexts/AppContext";
+import {Store} from "./store/contexts/AppContext";
 
 // pages
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Example from "./pages/Example";
 import Docs from "./pages/Docs";
+import Team from "./pages/Team";
 
 function App() {
-  const { theme } = Store();
+    const {theme} = Store();
 
-  return (
-    <ThemeProvider theme={theme ? Themes.light : Themes.dark}>
-      <GLobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/example" element={<Example />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/docs/*" element={<Docs />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme ? Themes.light : Themes.dark}>
+            <GLobalStyles/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/example" element={<Example/>}/>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/docs/*" element={<Docs/>}/>
+                    <Route path="/team" exact element={<Team/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
