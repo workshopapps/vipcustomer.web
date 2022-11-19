00f0e3d71cc3ad04a3af1fe891cd46b835c99cba
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-11-19 06:53:25 +0100

    refactor: moved input styles to new file

--- src/components/auth/Input.js
+++ src/components/auth/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
+import { Label, InputStyles, Error } from "./Input.styled";
 
 function Input({ label, id, errorMessage, isError, ...rest }) {
   return (
@@ -20,48 +20,3 @@ Input.propTypes = {
 };
 
 export default Input;
-
-const Label = styled.label`
-  font-size: 1.4rem;
-  font-weight: 400;
-  color: black;
-
-  @media only screen and (max-width: 480px) {
-    font-size: 1.2rem;
-  }
-`;
-
-const InputStyles = styled.input`
-  display: block;
-  margin-block: 0.6rem;
-  border: 1px solid #adb1bf;
-  border-radius: 8px;
-  padding: 1rem 1.6rem;
-  cursor: text;
-  width: 100%;
-  color: black;
-  font-size: 1.4rem;
-
-  @media only screen and (max-width: 480px) {
-    font-size: 1.2rem;
-  }
-
-  &:focus {
-    border: 1px solid #091540;
-  }
-
-  &::placeholder {
-    color: #adb1bf;
-  }
-`;
-
-const Error = styled.p`
-  color: red;
-  font-size: 1.4rem;
-  color: #ff414e;
-  font-weight: 400;
-
-  @media only screen and (max-width: 480px) {
-    font-size: 1.2rem;
-  }
-`;
