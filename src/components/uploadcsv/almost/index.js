9a0a6d58c8640fe7f9724aaab9a9690756d2f73a
Kennybabs-py <babalolakehinde74@gmail.com>
2022-11-19 18:19:09 +0100

    update: refactor of switch render

--- src/components/uploadcsv/almost/index.js
+++ src/components/uploadcsv/almost/index.js
@@ -3,14 +3,37 @@ import PropTypes from "prop-types";
 import ProgressBar from "components/progressbar";
 
 import styles from "./almost.module.css";
+import dummmy_dp from "../assets/dummy_dp.png";
 
 export default function Almost(props) {
   const { step } = props;
   return (
     <div className={styles.almost}>
-      <ProgressBar step={step} />
+      <div className={styles.header}>
+        <img src={dummmy_dp} alt="user" />
 
-      <div></div>
+        <div>
+          <h2>Almost</h2>
+        </div>
+      </div>
+
+      <div className={styles.upload__process}>
+        <ProgressBar step={step} />
+
+        <div className={styles.upload__info}>
+          <div className="waiting">
+            <div>
+              <img src="" alt="" /> <span>Info</span>
+            </div>
+
+            <span>
+              <img src="" alt="" />
+            </span>
+          </div>
+
+          <div className="sucess"></div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/uploadcsv/upload/index.js
+++ src/components/uploadcsv/upload/index.js
@@ -2,11 +2,13 @@ import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
 
 import styles from "./upload.module.css";
+import dummmy_dp from "../assets/dummy_dp.png";
 import CloudUploadIcon from "../assets/CloudUploadIcon.svg";
 import UploadIcon from "../assets/UploadIcon.svg";
 
 function Upload(props) {
   const { onNext } = props;
+
   const [selectedFile, setSelectedFile] = useState();
   const [isSelected, setIsSelected] = useState(false);
 
@@ -30,49 +32,60 @@ function Upload(props) {
 
   return (
     <div className={styles.upload}>
-      <div className={styles.texts}>
-        <h2>Upload Document</h2>
-        <p>Attach your CSV documemts for analysis</p>
+      <div className={styles.welcome__header}>
+        <img src={dummmy_dp} alt="user" />
+
+        <div>
+          <h2>Hi, Iyanu</h2>
+          <h2>Welcome back!</h2>
+        </div>
       </div>
 
-      <form className={styles.upload__form}>
-        <div className={styles.input__upload}>
-          {isSelected ? (
-            <div className={styles.uploaded_csv__info}>
-              <p>Filename: {selectedFile.name}</p>
-            </div>
-          ) : (
-            <div className={styles.no_csv__texts}>
-              <img src={CloudUploadIcon} alt="Upload" />
-              <h3>Drag and Drop your files here</h3>
-              <small>csv or pdf</small>
-            </div>
-          )}
-
-          <button onClick={handleClick} className={styles.browse_btn}>
-            Browse Files
-          </button>
-
-          <input
-            type="file"
-            name="file"
-            id="csvFileInput"
-            accept=".csv"
-            onChange={handleChange}
-            style={{ display: "none" }}
-            ref={hiddenFileInput}
-          />
+      <div className={styles.upload__process}>
+        <div className={styles.texts}>
+          <h2>Upload Document</h2>
+          <p>Attach your CSV documemts for analysis</p>
         </div>
 
-        <button
-          className={styles.upload_btn}
-          onClick={(e) => {
-            handleSubmit(e);
-          }}>
-          <img src={UploadIcon} alt="Upload" />
-          Upload
-        </button>
-      </form>
+        <form className={styles.upload__form}>
+          <div className={styles.input__upload}>
+            {isSelected ? (
+              <div className={styles.uploaded_csv__info}>
+                <p>Filename: {selectedFile.name}</p>
+              </div>
+            ) : (
+              <div className={styles.no_csv__texts}>
+                <img src={CloudUploadIcon} alt="Upload" />
+                <h3>Drag and Drop your files here</h3>
+                <small>csv or pdf</small>
+              </div>
+            )}
+
+            <button onClick={handleClick} className={styles.browse_btn}>
+              Browse Files
+            </button>
+
+            <input
+              type="file"
+              name="file"
+              id="csvFileInput"
+              accept=".csv"
+              onChange={handleChange}
+              style={{ display: "none" }}
+              ref={hiddenFileInput}
+            />
+          </div>
+
+          <button
+            className={styles.upload_btn}
+            onClick={(e) => {
+              handleSubmit(e);
+            }}>
+            <img src={UploadIcon} alt="Upload" />
+            Upload
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
