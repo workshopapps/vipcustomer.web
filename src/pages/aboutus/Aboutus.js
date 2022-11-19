706bc3a9b7d0592f4eb8d125b675bfaf94e4a8d0
Nyerhowho9 <nyerhowho9@gmail.com>
2022-11-19 14:36:57 +0100

    feat(FRO-5): created Aboutbottom.js component and added responsiveness

--- src/pages/aboutus/Aboutus.js
+++ src/pages/aboutus/Aboutus.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Abouttop from "components/about/Abouttop";
 import Aboutmain from "components/about/Aboutmain";
+import Aboutbottom from "components/about/Aboutbottom";
 
 const Aboutus = () => {
   return (
     <div>
       <Abouttop />
       <Aboutmain />
+      <Aboutbottom />
     </div>
   );
 };
