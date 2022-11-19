12455ce0148c748e0090d4696b4a721c252c1581
Kennybabs-py <babalolakehinde74@gmail.com>
2022-11-19 04:36:23 +0100

    feat: setup of csv upload page

---
+++ src/components/progressbar/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./styles.css";
+
+const progressData = [
+  { id: 0, image: "activeIcon", status: "active" },
+  { id: 1, image: "activeIcon", status: "active" },
+  { id: 2, image: "activeIcon", status: "active" },
+  { id: 3, image: "activeIcon", status: "active" }
+];
+
+export default function ProgressBar(props) {
+  const { step } = props;
+
+  return (
+    <div className="progressbar">
+      {progressData.map((item) => {
+        return (
+          <div key={item.id} className="progress-step">
+            {/* image */}
+            <div
+              className="image"
+              style={{
+                backgroundColor:
+                  step === item.id ? "blue" : step > item.id ? "green" : "red",
+                scale: step === item.id && "1.06",
+                zIndex: step === item.id && "5"
+              }}></div>
+            {/* line */}
+            <div
+              className="line"
+              style={{ display: item.id === 3 ? "none" : "" }}></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+ProgressBar.propTypes = {
+  step: PropTypes.number
+};
---
+++ src/components/uploadcsv/almost/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+import styles from "./almost.module.css";
+
+export default function Almost() {
+  return <div className={styles.almost}>AlmostThere</div>;
+}
