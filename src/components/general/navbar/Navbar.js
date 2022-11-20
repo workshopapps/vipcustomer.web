6e75978b209ecf0888f1b81766ccb4971e0d45ba
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-11-20 10:29:18 +0100

    fix: bugs stated on group

--- src/components/general/navbar/Navbar.js
+++ src/components/general/navbar/Navbar.js
@@ -71,8 +71,8 @@ implementation should be changed
           <Link>About Us</Link>
         </Items>
         <Items className="nav--link--items" tablet={tablet}>
-          <Link>Log in</Link>
-          <Link>
+          <Link to="/login">Log in</Link>
+          <Link to="/signup">
             {/* this a reusable button component */}
             <Button
               style={{ padding: "12px 24px", fontWeight: "700" }}
@@ -90,8 +90,8 @@ implementation should be changed
           <Link>Products</Link>
           <Link>Resourses</Link>
           <Link>About Us</Link>
-          <Link>Log in</Link>
-          <Link>Get Started</Link>
+          <Link to="/login">Log in</Link>
+          <Link to="/signup">Get Started</Link>
         </MobileNavWrapper>
       )}
     </NavWrapper>
