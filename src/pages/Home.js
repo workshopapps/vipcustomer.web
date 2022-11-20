d4df70cf2aaed5287d8de03790aaf827898d6edc
TomisinAjayi <thomieajayi@gmail.com>
2022-11-20 14:42:46 +0100

    the logout page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,27 @@ import LandingPage from "../components/landingpage/LandingPage";
 import useScreenSize from "../hooks/useScreenSize";
 import { Navbar, Footer } from "components/general";
 // import Logout from "./Logout";
+// import for logout
+import Logout from "components/auth/logout";
+import { Button } from "react-bootstrap";
+// logout import ends here
 
 const Home = () => {
   const { screenWidth } = useScreenSize();
   const mobile = screenWidth <= 690;
   const tablet = screenWidth <= 1024;
 
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <div>
       <Navbar mobile={mobile} tablet={tablet} />
+      {/* the button for logout  */}
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Logout
+      </Button>
 
+      <Logout show={modalShow} onHide={() => setModalShow(false)} />
+      {/* /logout button ends here  */}
       <LandingPage />
       <Footer mobile={mobile} tablet={tablet} />
 
