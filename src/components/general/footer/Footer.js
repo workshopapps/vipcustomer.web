d39ffb296e90412573d3b8159f1c335ad4bd3c6b
alex chika <mcluckey1@gmail.com>
2022-11-20 14:20:12 +0100

    updated navbar routing

--- src/components/general/footer/Footer.js
+++ src/components/general/footer/Footer.js
@@ -13,23 +13,31 @@ const Footer = () => {
         <div className="footbarone">
           <h4>Features</h4>
           <Linkbtn href="#" child="Axe API Feature" classname="footlink" />
-          <Linkbtn href="#" child="How it works" classname="footlink" />
+          <Linkbtn href="/demo" child="How it works" classname="footlink" />
         </div>
         <div className="footbarone">
           <h4>Help & Support</h4>
-          <Linkbtn href="#" child="FAQ" classname="footlink" />
-          <Linkbtn href="#" child="Help Center" classname="footlink" />
+          <Linkbtn href="/faq" child="FAQ" classname="footlink" />
+          <Linkbtn
+            href="/help-ceter"
+            child="Help Center"
+            classname="footlink"
+          />
         </div>
         <div className="footbarone">
           <h4>Terms & Condition</h4>
-          <Linkbtn href="#" child="Privacy Policy" classname="footlink" />
+          <Linkbtn
+            href="/privacy"
+            child="Privacy Policy"
+            classname="footlink"
+          />
         </div>
         <div className="footbarone">
           <h4>Company</h4>
-          <Linkbtn href="#" child="About us" classname="footlink" />
-          <Linkbtn href="#" child="Meet our team" classname="footlink" />
+          <Linkbtn href="/about-us" child="About us" classname="footlink" />
+          <Linkbtn href="/meet" child="Meet our team" classname="footlink" />
           <Linkbtn
-            href="./mission"
+            href="/mission"
             child="Vision & Mission"
             classname="footlink"
           />
