b99e18b71c405ae9b6d2b91977f628f4edab016b
alex chika <mcluckey1@gmail.com>
2022-11-24 14:55:26 +0100

    reviewed and refactor

--- src/components/landingpage/image.styled.js
+++ src/components/landingpage/image.styled.js
@@ -10,15 +10,68 @@ const VideoWrapper = styled.div`
 `;
 
 const VideoContainer = styled.div`
-  max-width: 982px;
-  width: 100%;
-  max-height: 742px;
+  max-width: 750px;
+  margin: 0 auto;
+  aspect-ratio: 16/9;
   height: 100%;
+  position: relative;
+  color: white;
   & > video {
-    background: green;
     width: 100%;
-    height: auto;
+    height: 100%;
     object-fit: cover;
+    background-color: pink;
+  }
+
+  .video__overlay {
+    background: #091540;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    visibility: visible;
+    opacity: 1;
+    transition: all 0.3s linear;
+
+    button {
+      height: 100px;
+      width: 100px;
+      border-radius: 50%;
+      position: absolute;
+      opacity: 0.65;
+      z-index: 2;
+    }
+
+    h2 {
+      font-size: 1.8rem;
+    }
+    p {
+      font-size: 1.3rem;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    @media screen and (min-width: 500px) {
+      h2 {
+        font-size: 2.8rem;
+      }
+      p {
+        font-size: 2rem;
+      }
+    }
+  }
+
+  .video__overlay.close {
+    opacity: 0;
+    visibility: hidden;
   }
 `;
 
