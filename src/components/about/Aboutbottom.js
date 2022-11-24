cc3bad1bb1344e6511b0742feafa649df0247410
alex chika <mcluckey1@gmail.com>
2022-11-24 16:06:02 +0100

    reviewed and refactored

--- src/components/about/Aboutbottom.js
+++ src/components/about/Aboutbottom.js
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./About.module.css";
 import { useNavigate } from "react-router-dom";
+import { AuthStore } from "store/contexts/AuthContext";
 
 const Aboutbottom = () => {
+  const { user } = AuthStore();
 
   let navigate = useNavigate();
-  const navigateLogin = () =>{
-    navigate('/login');
-  }
+
+  const navigateLogin = () => {
+    let path;
+    if (user) {
+      path = "/dashboard";
+    } else {
+      path = "/login";
+    }
+    navigate(path);
+  };
 
   return (
     <div className={styles.bottCont}>
@@ -18,7 +27,15 @@ const Aboutbottom = () => {
             See how AxeAPI can help your business leverage customer information,
             to make more informed business development decisions today
           </h3>
-          <button className={styles.signupBtn} onClick={navigateLogin} >Click Here to Sign Up</button>
+          {user ? (
+            <button className={styles.signupBtn} onClick={navigateLogin}>
+              Go to Dashboard
+            </button>
+          ) : (
+            <button className={styles.signupBtn} onClick={navigateLogin}>
+              Get Started
+            </button>
+          )}
         </div>
       </div>
     </div>
