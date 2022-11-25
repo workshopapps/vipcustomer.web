a2466bf85cf7c5319b9ceaa04b1c0ff7481adc2d
Kennybabs-py <babalolakehinde74@gmail.com>
2022-11-25 14:54:14 +0100

    feat(FRO-29): Persistence of Entries, Display of Error and Loading UI states, Api refactor

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,24 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import NavBar from "./components/navbar";
 import Upload from "./components/upload";
 import Almost from "./components/almost";
 import VipResult from "./components/vip-result";
+
+//hooks
 import axios from "./components/api";
+import { useLocalStorage } from "hooks/useLocalStorage";
 
 import styles from "./dashboard.module.css";
 
 export default function Dashboard() {
   const [step, setStep] = useState(0);
   const [namesData, setNamesData] = useState([]);
-  const [isUploading, setIsUploading] = useState(false);
+  const [uploadState, setUploadState] = useState({
+    isUploading: false,
+    uploadError: false
+  });
   const [responseData, setResponseData] = useState({});
+  const [username, setUsername] = useState("");
+
+  //localStorage hook
+  const [storedValue, setValue] = useLocalStorage("storedEntries", []);
 
   const onChange = (nextStep) => {
     setStep(nextStep < 0 ? 0 : nextStep > 3 ? 3 : nextStep);
   };
   const onNext = () => onChange(step + 1);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUser = JSON.parse(localStorage.getItem("user"));
+      const user = storedUser.user;
+      setUsername(user.first_name);
+    }
+    console.table(storedValue);
+    setNamesData(storedValue);
+  }, []);
+
+  //Adds entry to list
   function handleNamesData(data) {
     const newEntry = {
       name: data.name,
@@ -30,29 +51,39 @@ export default function Dashboard() {
     setNamesData([...namesData, newEntry]);
   }
 
+  //Removes entry from lists
   function removeEntry(id) {
     const filteredEntries = namesData.filter((_, index) => index !== id);
     setNamesData(filteredEntries);
+    localStorage.removeItem("storedEntries");
   }
 
   //Upload entries function
   async function handleUpload() {
-    setIsUploading(true);
-    setStep(1);
-    console.log(JSON.stringify(namesData));
+    setUploadState({ ...uploadState, isUploading: true, uploadError: false });
+    setValue(namesData);
 
-    try {
-      const { data } = await axios.post("/api/search/search-many", {
-        data: namesData
-      });
+    setTimeout(async () => {
+      setStep(1);
 
-      setResponseData(data);
-      console.log(data);
-      setStep(2);
-    } catch (error) {
-      console.log(error?.response);
-      setStep(0);
-    }
+      try {
+        const { data } = await axios.post("/api/search/search-many", {
+          data: namesData
+        });
+        setUploadState({ ...uploadState, isUploading: false });
+        setResponseData(data);
+        console.log(data);
+        setStep(2);
+      } catch (error) {
+        console.log(error?.response);
+        setUploadState({
+          ...uploadState,
+          uploadError: true,
+          isUploading: false
+        });
+        setStep(0);
+      }
+    }, 2000);
   }
 
   return (
@@ -66,24 +97,26 @@ export default function Dashboard() {
             case 0:
               return (
                 <Upload
+                  username={username}
                   onNext={onNext}
                   namesData={namesData}
                   handleNamesData={handleNamesData}
                   removeEntry={removeEntry}
                   handleUpload={handleUpload}
+                  uploadState={uploadState}
                 />
               );
 
             case 1:
-              return (
-                <Almost onNext={onNext} step={step} isUploading={isUploading} />
-              );
+              return <Almost username={username} onNext={onNext} step={step} />;
 
             case 2:
               return <Almost onNext={onNext} step={step} />;
 
             case 3:
-              return <VipResult responseData={responseData} />;
+              return (
+                <VipResult username={username} responseData={responseData} />
+              );
             default:
               return null;
           }
