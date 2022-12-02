a02808075c7dd7bd117a0a282e45a21c3db6bc1d
Efoneli <abbaefoneli@gmail.com>
2022-12-03 05:02:07 +1030

    changes in document

--- src/pages/Documents.js
+++ src/pages/Documents.js
@@ -6,15 +6,15 @@ import SN1 from "pages/docs/snippet1.png";
 
 const Documents = () => {
   return (
-
     <div>
     <h1>
       Introduction
     </h1>
     <h5>
-      The StartFinder API can be used by virtually any company or firm who wants to have a track of VIPs which involves peoples of fames, wealth and importance. We offer a spectrum of models with differentiate the levels of individuals and which can be suitable for different sales for company growth, as well as the ability to have the idea of people of power who have visited your website. These models can be used for everything from sales generation to customer appreciation and also to customer classification. </h5>
+      The StartFinder API can be used by virtually any company or firm who wants to have a track of VIPs which involves peoples of fames, wealth and importance. We offer a spectrum of models with differentiate the levels of individuals and which can be suitable for different sales for company growth, as well as the ability to have the idea of people of power who have visited your website. These models can be used for everything from sales generation to customer appreciation and also to customer classification. 
+      <br /> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa laborum dignissimos explicabo, sed exercitationem aut fuga praesentium harum cumque molestiae delectus veniam? Nostrum, ducimus tenetur cumque expedita debitis dolore libero?
+    </h5>
   </div>
-
   );
 };
 
