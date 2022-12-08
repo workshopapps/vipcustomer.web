20be685ab36d77e88a10113dfc71bbd8c45ef611
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-12-08 15:11:45 +0100

    feat: access and refresh tokens

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,38 @@
 import axios from "axios";
-// import { AuthStore } from "store/contexts/AuthContext";
+import { AuthStore } from "store/contexts/AuthContext";
+import { login_a } from "store/actions/authActions";
 
 const user = JSON.parse(localStorage.getItem("user")) || {};
-let token = user?.access_token;
+const accessToken = user?.access_token;
+const refreshToken = user?.refresh_token;
 
-export default axios.create({
+const request = axios.create({
   // baseURL: "http://50.18.54.50:8000",
   baseURL: "https://api.starfinder.hng.tech",
-  headers: { Authorization: "bearer " + token }
+  headers: { Authorization: "bearer " + accessToken }
 });
+
+export default request;
+
+request.interceptors.response.use(
+  (response) => response,
+  async function (error) {
+    if (error?.response?.status === 403 && refreshToken) {
+      const previousRequest = error?.config;
+      const newAccessToken = await request.get("/api/user/refresh_token", {
+        headers: { Authorization: "bearer " + refreshToken }
+      });
+      previousRequest.headers["Authorization"] = "bearer " + newAccessToken;
+      updateLocalStorageAndContextState(newAccessToken);
+      return request(previousRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+function updateLocalStorageAndContextState(newAccessToken) {
+  user.accessToken = newAccessToken;
+  const { dispatch } = AuthStore;
+  login_a(dispatch, user);
+}
