574703dfc9ec17bccc0eaafce278da470f719f93
alex chika <mcluckey1@gmail.com>
2022-12-08 10:04:22 +0100

    reduced navbar height, deleted unused files

--- src/components/general/navbar/navbar.styled.js
+++ src/components/general/navbar/navbar.styled.js
@@ -10,8 +10,7 @@ const NavWrapper = styled.nav`
   color: white;
   font-weight: 500;
   display: flex;
-  height: 65px;
-
+  height: 50px;
   padding-inline: ${({ mobile, tablet }) =>
     mobile ? "40px" : tablet ? "78.9px" : "120px"};
   width: 100%;
@@ -28,18 +27,12 @@ const NavItemsWrapper = styled.ul`
   align-items: center;
 `;
 const LogoWrapper = styled.div`
-  width: ${({ mobile, tablet }) =>
-    mobile ? "48.9%" : tablet ? "73.46%" : "164.63px"};
-  height: ${({ mobile, tablet }) =>
-    mobile ? "48.9%" : tablet ? "73.46%" : "49px"};
+  width: 100%;
+  height: 50px;
   & > img {
     width: 100%;
     height: 100%;
     object-fit: cover;
-  }
-  > img {
-    color: white;
-    width: 85%;
   }
 `;
 
