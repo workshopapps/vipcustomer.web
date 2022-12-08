20fcc0bd4a7ba0bff506ac5957b2e4fd0b277eec
dumebi.orji1@yahoo.com <morji594@gmail.com>
2022-12-08 23:01:12 +0100

    fixed minor bugs

--- src/components/dashboard/profile/assets/User.jsx
+++ src/components/dashboard/profile/assets/User.jsx
@@ -9,9 +9,9 @@ const User = ({ setKeyIsVisible, keyIsVisible }) => {
       }}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
       className="w-6 h-6">
--- src/components/dashboard/profile/index.js
+++ src/components/dashboard/profile/index.js
@@ -1,12 +1,13 @@
 // please export your page here
 import React from "react";
 import Input from "./Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./index.module.css";
 import axios from "api/axios";
 import { useEffect, useState } from "react";
 import Key from "./assets/Key";
 import User from "./assets/User";
+import ErrorModal from "./ErrorModal";
 
 const index = () => {
   const [userState, setUserState] = useState({
@@ -15,13 +16,19 @@ const index = () => {
     email: ""
   });
   const [apiKey, setApiKey] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
   const [keyIsVisible, setKeyIsVisible] = useState(false);
+  const nav = useNavigate();
 
   const getData = async (user) => {
-    const data = await axios.get(
-      `/api/user/get_single_user?user_id=${user.id}`
-    );
-    setUserState(data.data.data);
+    try {
+      const data = await axios.get(
+        `/api/user/get_single_user?user_id=${user.id}`
+      );
+      setUserState(data.data.data);
+    } catch (error) {
+      setErrorMsg(error.message);
+    }
   };
 
   const getApiKey = async () => {
@@ -38,64 +45,71 @@ const index = () => {
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user")).user;
+    if (!user) nav("/login");
     getData(user);
     getApiKey();
-  }, []);
+  }, [JSON.parse(localStorage.getItem("user")).user]);
 
   return (
-    <div className={styles.profile}>
-      <div className={styles.profile__header}>
-        <div className={styles.profile__header_details}>
-          <h1>Profile</h1>
-          <Link to="/dashboard/settings">Edit Profile</Link>
-        </div>
-        <div className={styles.profile__header_img}>
-          {getUserImg(userState.first_name, userState.last_name)}
-        </div>
-      </div>
-      <div className={styles.profile__details}>
-        <div>
-          <Input label="First Name">{userState.first_name}</Input>
-          <Input label="Last Name">{userState.last_name}</Input>
-        </div>
-        <Input label="email">{userState.email}</Input>
-      </div>
-      <div className={styles.profile__keys}>
-        <div>
-          <span className={styles.profile__keys_img}>
-            <Key />
-          </span>
-          <div className={styles.profile__keys_details}>
-            <h4>API KEY</h4>
-            <p>A Private API Key to interact with the StarFinder API</p>
+    <>
+      {errorMsg !== "" ? (
+        <ErrorModal message={errorMsg} />
+      ) : (
+        <div className={styles.profile}>
+          <div className={styles.profile__header}>
+            <div className={styles.profile__header_details}>
+              <h1>Profile</h1>
+              <Link to="/dashboard/settings">Edit Profile</Link>
+            </div>
+            <div className={styles.profile__header_img}>
+              {getUserImg(userState.first_name, userState.last_name)}
+            </div>
+          </div>
+          <div className={styles.profile__details}>
             <div>
-              <input
-                type={keyIsVisible ? "text" : "password"}
-                readOnly
-                value={apiKey}
-              />
-              <User
-                setKeyIsVisible={setKeyIsVisible}
-                keyIsVisible={keyIsVisible}
-              />
+              <Input label="First Name">{userState.first_name}</Input>
+              <Input label="Last Name">{userState.last_name}</Input>
+            </div>
+            <Input label="email">{userState.email}</Input>
+          </div>
+          <div className={styles.profile__keys}>
+            <div>
+              <span className={styles.profile__keys_img}>
+                <Key />
+              </span>
+              <div className={styles.profile__keys_details}>
+                <h4>API KEY</h4>
+                <p>A Private API Key to interact with the StarFinder API</p>
+                <div>
+                  <input
+                    type={keyIsVisible ? "text" : "password"}
+                    readOnly
+                    value={apiKey}
+                  />
+                  <User
+                    setKeyIsVisible={setKeyIsVisible}
+                    keyIsVisible={keyIsVisible}
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <span className={styles.profile__keys_img}>
+                <Key />
+              </span>
+              <div className={styles.profile__keys_details}>
+                <h4>APPLICATION IDENTIFIER</h4>
+                <p>
+                  A Public Key Identifier that can be exposed to client&apos;s
+                  applications
+                </p>
+                <p>2849-3934-430</p>
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          <span className={styles.profile__keys_img}>
-            <Key />
-          </span>
-          <div className={styles.profile__keys_details}>
-            <h4>APPLICATION IDENTIFIER</h4>
-            <p>
-              A Public Key Identifier that can be exposed to client&apos;s
-              applications
-            </p>
-            <p>2849-3934-430</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
