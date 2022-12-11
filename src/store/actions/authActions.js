a4aec080ca0b674458c3871bd71e4726b2396c4f
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-12-11 13:05:36 +0100

    feat: refresh and access token new implementation

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,14 +1,18 @@
 import * as types from "../actiontypes/authTypes";
 
-export const login_a = (dispatch, payload) => {
+export const login_a = (dispatch, payload, saveTimeStamp = true) => {
   dispatch({ type: types.ON_LOGIN_SUCCESSFUL, payload });
   localStorage.setItem("user", JSON.stringify(payload));
+
+  if (saveTimeStamp)
+    localStorage.setItem("access_token_timestamp", new Date().getTime());
 };
 
 export const logout_a = (dispatch) => {
   dispatch({ type: types.ON_LOGOUT });
   localStorage.removeItem("user");
   localStorage.removeItem("api_key");
+  localStorage.removeItem("access_token_timestamp");
 };
 
 export const setApiKey_a = (dispatch, payload) => {
